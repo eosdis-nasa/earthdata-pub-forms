fc001b44757eb13cbbb25bb688a21ee8a0c59c25
Wright <dw2@ornl.gov>
2020-03-31 09:34:05 -0400

    variables now in their own tf files

--- main.tf
+++ main.tf
@@ -6,11 +6,3 @@ resource "aws_s3_bucket" "example" {
   bucket = "${var.prefix}-terraform-example"
   acl    = "private"
 }
-
-variable "aws_region" {
-  type = string
-}
-
-variable "prefix" {
-  type = string
-}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
