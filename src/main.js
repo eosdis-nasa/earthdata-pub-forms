7540554b7e3c86d3e99de491324d6646bb1e5244
Wright <dw2@ornl.gov>
2020-04-06 14:53:52 -0400

    Revert "in progress attempt to add undo/redo plugin"
    This reverts commit 4cc7cea0a588970025ecf7f8c0c3e910f37da149.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vuelidate from 'vuelidate'
-import VuexUndoRedo from './plugin';
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
@@ -17,8 +16,6 @@ Vue.use(BootstrapVueIcons)
 Vue.use(IconsPlugin)
 // Optionally install vuelidatejs plugin
 Vue.use(Vuelidate)
-// Install Undo/Redo mixin
-Vue.use(VuexUndoRedo);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
--- src/store/index.js
+++ src/store/index.js
@@ -12,23 +12,10 @@ Vue.use(VuexUndoRedo);
 export const store = new Vuex.Store({
   // State is the default obj and value
   state: {
-    question_answers: []
   },
   mutations: {
-    // push question state to save the payload to the store state question_answers
-    pushQuestionsState(state, payload){
-      state.question_answers.push(Object.assign({}, payload))
-    },
-    // .emptyState() is needed by VuexUndoRedo
-    emptyState() {
-      this.replaceState({ question_answers: [] });       
-    },
   },
   actions: {
-    
-  },
-  getters: {
-
   },
   modules: {
   }
