d0174d36c3678b10e465419e4ce3a8a0ba735365
Kimberly Broughton <broughtonkk@ornl.gov>
2020-04-06 14:21:18 -0400

    remove plugin stuff

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vuelidate from 'vuelidate'
-import VuexUndoRedo from './plugin';
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
