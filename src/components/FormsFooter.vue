6ae2bba97a23dad915efec97a2d329c459d07d44
Kimberly Broughton <broughtonkk@ornl.gov>
2024-04-18 13:45:49 -0400

    mobile improvements

--- src/components/FormsFooter.vue
+++ src/components/FormsFooter.vue
@@ -7,8 +7,6 @@
           <li><a href="https://www.nasa.gov/FOIA/index.html" target="_blank" alt="FOIA" title="FOIA">FOIA</a></li>
           <li><a href="https://www.nasa.gov/about/highlights/HP_Privacy.html" target="_blank" alt="NASA Privacy Policy" title="NASA Privacy Policy">NASA Privacy Policy</a></li>
           <li><a href="https://www.usa.gov/" target="_blank" alt="USA dot gov" title="USA dot gov">USA.gov</a></li>
-          <li><a href="javascript:feedback.showForm();" alt="Feedback Form" title="Feedback Form">Feedback</a></li>
-          <!-- <li style="float: right"><span class="version badge eui-badge--md">v 1.193.3-r22.1.3</span></li> -->
         </ul>
       </nav>
     </div>
